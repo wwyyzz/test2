5b393c0626f57803d9f643a7312f0dcb5587c837
wwyyzz <wwyyzz08@sina.com>
2016-11-20 00:03:38 +0800

    new project started

---
+++ Ui_test2.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\MyCode\test2\test2.ui'
+#
+# Created by: PyQt5 UI code generator 5.7
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(400, 300)
+        Dialog.setSizeGripEnabled(True)
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(60, 170, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2.setGeometry(QtCore.QRect(220, 170, 75, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(120, 80, 91, 31))
+        self.label.setObjectName("label")
+
+        self.retranslateUi(Dialog)
+        self.pushButton_2.clicked.connect(Dialog.close)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.pushButton.setText(_translate("Dialog", "确定"))
+        self.pushButton_2.setText(_translate("Dialog", "关闭"))
+        self.label.setText(_translate("Dialog", "显示"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
---
+++ test2.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module implementing Dialog.
+"""
+import sys
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QDialog,  QApplication
+
+from Ui_test2 import Ui_Dialog
+
+
+class Dialog(QDialog, Ui_Dialog):
+    """
+    Class documentation goes here.
+    """
+    def __init__(self, parent=None):
+        """
+        Constructor
+        
+        @param parent reference to the parent widget
+        @type QWidget
+        """
+        super(Dialog, self).__init__(parent)
+        self.setupUi(self)
+    
+    @pyqtSlot()
+    def on_pushButton_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        self.label.setText("This is my first app")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    dlg = Dialog()
+    dlg.show()
+    sys.exit(app.exec_())
